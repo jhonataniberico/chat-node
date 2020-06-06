4dd096e5a8e6a6f9b9f12cd08d72c43e1ac65d16
CAV <christianayala@modulomultitest.com>
2020-06-05 20:27:20 -0500

    cambios

--- routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app, participants) {
-  app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/index.html');
-  });
-  app.get('/participants', function(req, res) {
-    res.send(participants);
-  });
-}
